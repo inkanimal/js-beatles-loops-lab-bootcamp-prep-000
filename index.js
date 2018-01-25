7bb10c4feb8160f613f580d4a11689a4c4b742a6
geoffrey woodcock <theatomproject@gmail.com>
2018-01-25 20:16:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+//var musicians = ["John Lennon" "Paul McCartney" "George Harrison" "Ringo Starr"]
+//var instruments = ["Guitar" "Bass Guitar" "Lead Guitar" "Drums"]
+
+function theBeatlesPlay(musicians, instruments){
+  var newString = [];
+  for (var i = 0; i < musicians.length; i ++){
+  newString.push(musicians[i] + " plays " + instruments[i]);
+
+}
+return newString
+}
